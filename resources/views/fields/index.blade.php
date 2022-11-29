8df0d437674434acf756236825ec4f5f581951ca
Pascal Hurni <phi@pragmaticpurist.org>
2022-11-29 14:45:43 +0100

    Add the value_kind to the list of fields

--- resources/views/fields/index.blade.php
+++ resources/views/fields/index.blade.php
@@ -3,6 +3,8 @@
     @foreach ($fields as $field)
     <li>
         {{ $field->label }}
+        <br>
+        {{ $field->value_kind->name }}
         <form action="{{ route('exercises.fields.destroy', compact('exercise', 'field')) }}" method="POST">
             @method('DELETE')
             @csrf
